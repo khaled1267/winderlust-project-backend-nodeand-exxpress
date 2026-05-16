aa03e1c9a21735ace55a6f0c46473cb58be69a10
khaled1267 <khaledkhan1267@gmail.com>
2026-05-16 21:14:46 +0600

    currention code

--- index.js
+++ index.js
@@ -71,8 +71,8 @@ const varifytoken = async(req, res, next)=>{
      
     }
 
-async function run() {
-  try {
+// async function run() {
+  // try {
     // await client.connect();
 
     const db= client.db("wanderlust")
@@ -148,12 +148,12 @@ async function run() {
 
     
     // await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // await client.close();
-  }
-}
-run().catch(console.dir);
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } finally {
+//     // await client.close();
+//   }
+// }
+// run().catch(console.dir);
 app.get('/', (req, res) => {
   res.send('hello api is running')
 })
